745dfa90b20c61c1423a458b097567a205831ee6
Dep0sit <ashkarmalek0@gmail.com>
2021-01-07 07:46:19 -0500

    :bug: Added a check for admin when accepting and denying reports

--- src/events/reportReactions.ts
+++ src/events/reportReactions.ts
@@ -1,6 +1,7 @@
 import { MessageReaction, TextChannel, User } from "discord.js";
 import Event, { EventNameType } from ".";
 import config from "../config";
+import { AdminModel } from "../models/admin";
 import { ReportModel } from "../models/report";
 import { Scammer, ScammerModel } from "../models/scammer";
 import embeds from "../utils/embeds";
@@ -14,62 +15,64 @@ export default class ReportReactions extends Event {
 
     const message = reaction.message;
     if (message.guild.id === config.mainGuild) {
-      const reportData = await ReportModel.findOne({
-        reportMessageId: message.id,
-      });
+      const admin = await AdminModel.findOne({ userId: user.id });
+      if (admin || config.owners.includes(user.id)) {
+        const reportData = await ReportModel.findOne({
+          reportMessageId: message.id,
+        });
+        if (reportData) {
+          if (["❌", "✅"].includes(reaction.emoji.name)) {
+            const logChannel = message.guild.channels.resolve(
+              config.reportLogsChannel
+            ) as TextChannel;
 
-      const logChannel = message.guild.channels.resolve(
-        config.reportLogsChannel
-      ) as TextChannel;
+            if (reaction.emoji.name === "✅") {
+              const scammerData = await ScammerModel.findOne({
+                $or: [
+                  {
+                    ingameUuid: reportData.ingameUuid,
+                  },
+                ],
+              });
+              if (!scammerData) {
+                if (message.deletable) message.delete();
+                await reportData.deleteOne();
+                const scammer = await ScammerModel.create(
+                  new Scammer(
+                    reportData.userId,
+                    reportData.ingameName,
+                    reportData.ingameUuid,
+                    reportData.reason
+                  )
+                );
 
-      if (reportData) {
-        if (["❌", "✅"].includes(reaction.emoji.name)) {
-          if (reaction.emoji.name === "✅") {
-            const scammerData = await ScammerModel.findOne({
-              $or: [
-                {
-                  ingameUuid: reportData.ingameUuid,
-                },
-              ],
-            });
-            if (!scammerData) {
+                logChannel.send(
+                  embeds
+                    .normal(
+                      `Report Accepted`,
+                      `**${scammer.ingameName}** has been added to the scammer list.
+                    
+                    Scammer ID #${scammer.id}
+                    Reason: ${scammer.reason}`
+                    )
+                    .setFooter(`Proof provided below`)
+                    .setImage(message.embeds[0].image.url)
+                );
+              }
+            } else {
               if (message.deletable) message.delete();
               await reportData.deleteOne();
-              const scammer = await ScammerModel.create(
-                new Scammer(
-                  reportData.userId,
-                  reportData.ingameName,
-                  reportData.ingameUuid,
-                  reportData.reason
-                )
-              );
 
               logChannel.send(
-                embeds
-                  .normal(
-                    `Report Accepted`,
-                    `**${scammer.ingameName}** has been added to the scammer list.
-                  
-                  Scammer ID #${scammer.id}
-                  Reason: ${scammer.reason}`
-                  )
-                  .setFooter(`Proof provided below`)
-                  .setImage(message.embeds[0].image.url)
+                embeds.normal(
+                  `Report Denied`,
+                  `The report on **${reportData.ingameName}** has been denied.`
+                )
               );
             }
           } else {
-            if (message.deletable) message.delete();
             await reportData.deleteOne();
-
-            logChannel.send(
-              embeds.normal(
-                `Report Denied`,
-                `The report on **${reportData.ingameName}** has been denied.`
-              )
-            );
           }
-        } else {
-          await reportData.deleteOne();
         }
       }
     }
